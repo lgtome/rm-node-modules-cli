747d317a580518d32d20708b07d65f4036d1a2ad
lgtome <seryikotenok232@gmail.com>
2022-05-01 01:25:58 +0300

    clean up

--- __tests__/exceptions.test.js
+++ __tests__/exceptions.test.js
@@ -3,17 +3,17 @@ import { run } from '../src/service/index.js'
 
 const errorMessage = 'Arguments not provided!'
 
-test.only('Throw error when days not a number', async (t) => {
+test('Throw error when days not a number', async (t) => {
   const error = await run(`${process.cwd()}`, 'fs', 'check')
   t.is(error.message, errorMessage)
 })
 
-test.only('Throw error when path not provided', async (t) => {
+test('Throw error when path not provided', async (t) => {
   const error = await run(null, 'fs', 'check')
 
   t.is(error.message, errorMessage)
 })
-test.only('Throw error when type is incorrect', async (t) => {
+test('Throw error when type is incorrect', async (t) => {
   const error = await run(null, 'fs', null)
 
   t.is(error.message, errorMessage)
